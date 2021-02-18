9f012584d6328807f006f32a51fa50133c7fec66
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-02-18 16:43:45 +0300

    buycoordinator added

---
+++ CoordinatorTest/BuyCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  BuyCoordinator.swift
+//  CoordinatorTest
+//
+//  Created by DenizCagilligecit on 18.02.2021.
+//
+
+import UIKit
+
+class BuyCoordinator: Coordinator {
+    var children: [Coordinator] = []
+    weak var parentCoordinator: MainCoordinator?
+    var navigationController: UINavigationController
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    func didFinishBuying() {
+        parentCoordinator?.childDidFinish(child: self)
+    }
+    
+}
--- CoordinatorTest/MainCoordinator.swift
+++ CoordinatorTest/MainCoordinator.swift
@@ -24,15 +24,26 @@ class MainCoordinator: Coordinator {
         
     }
     func buySubscription() {
-        let vc = BuyViewController.instantiate()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
+        let child = BuyCoordinator(navigationController: navigationController)
+        child.parentCoordinator = self
+        children.append(child)
+        child.start()
+        
     }
 
     func createAccount() {
         let vc = CreateAccountViewController.instantiate()
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: true)
+    }
+    func childDidFinish(child: Coordinator? ) {
+        for (index, coordinator) in children.enumerated() {
+            if coordinator === child {
+                children.remove(at: index)
+                break
+            }
+        }
+        
     }
     
     
