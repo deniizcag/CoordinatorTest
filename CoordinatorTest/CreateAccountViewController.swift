e5374a7a80feca1fa92a5090f7dedb17146a01dd
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-02-18 15:00:00 +0300

    buy and createvc are added

---
+++ CoordinatorTest/CreateAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountViewController.swift
+//  CoordinatorTest
+//
+//  Created by DenizCagilligecit on 18.02.2021.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
